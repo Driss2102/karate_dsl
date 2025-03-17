f048d5dbf2d54afeef341feb73e51066064205e2
Driss <lahbilidriss@gmail.com>
2025-03-17 10:03:52 +0000

    add extent reports

--- src/main/java/com/karate/karatespring/controller/KarateTestController.java
+++ src/main/java/com/karate/karatespring/controller/KarateTestController.java
@@ -26,29 +26,40 @@ public class KarateTestController {
         );
     }
 
+    @GetMapping("/runTestWithParams")
+    public String runKarateTest(@RequestParam("username") String username,@RequestParam("password") String password,@RequestParam("expiresInMins") int expiresInMins) {
 
-    @GetMapping("/runreport")
-    public String runKarateTestWithrep() {
-        String reportDir = "target/extended-reports";
 
-        Results results = Runner.path("src/test/resources/karate/test.feature")
-                .outputCucumberJson(true)
-                .parallel(4);
+        System.out.println(" Requête reçue avec username: " + username + ", password: " + password + ", expiresInMins: " + expiresInMins);
 
-        //pour la partie du Extent Report
-        CustomExtentReport extentReport = new CustomExtentReport()
-                .withKarateResult(results)
-                .withReportDir(reportDir)
-                .withReportTitle("Karate Test Execution Report");
-        extentReport.generateExtentReport();
+        Results results = Runner.builder()
+                .path("src/test/resources/karate/testadd.feature")
+                .systemProperty("username", username)
+                .systemProperty("password", password)
+                .systemProperty("expiresInMins", String.valueOf(expiresInMins))
+                .reportDir("C:/Users/Lenovo/Desktop/karate-reports")
+                .outputCucumberJson(false)
+                .outputHtmlReport(true)
+                .outputJunitXml(false)
+
+                .parallel(1);
 
 
         return String.format(
-                "Scénarios Karate exécutés : %d réussis, %d échoués. Rapport généré dans target/karate-reports/ExtentReport.html",
+                "Scénarios Karate exécutés : %d réussis, %d échoués",
                 results.getScenariosPassed(), results.getScenariosFailed()
         );
     }
 
+    @GetMapping("/runCsv")
+    public String runKarateTestscsv() {
+        Results results = Runner.path("src/test/resources/karate/testcsv.feature").parallel(1);
+
+        return String.format(
+                "Scénarios Karate exécutés : %d réussis, %d échoués",
+                results.getScenariosPassed(), results.getScenariosFailed()
+        );
+    }
 
     @GetMapping("/runchoose")
     public String runChoose(@RequestParam(required = false, defaultValue = "") String tags) {
@@ -74,38 +85,24 @@ public class KarateTestController {
         );
     }
 
+    @GetMapping("/runReport")
+    public String runKarateTestWithrep() {
+        String reportDir = "target/extended-reports";
 
-    @GetMapping("/runTest")
-    public String runKarateTest(@RequestParam("username") String username,@RequestParam("password") String password,@RequestParam("expiresInMins") int expiresInMins) {
+        Results results = Runner.path("src/test/resources/karate/test.feature")
+                .outputCucumberJson(true)
+                .parallel(4);
 
-
-        System.out.println(" Requête reçue avec username: " + username + ", password: " + password + ", expiresInMins: " + expiresInMins);
-
-        Results results = Runner.builder()
-                .path("src/test/resources/karate/testadd.feature")
-                .systemProperty("username", username)
-                .systemProperty("password", password)
-                .systemProperty("expiresInMins", String.valueOf(expiresInMins))
-                .reportDir("C:/Users/Lenovo/Desktop/karate-reports")
-                .outputCucumberJson(false)
-                .outputHtmlReport(true)
-                .outputJunitXml(false)
-
-                .parallel(1);
+        //pour la partie du Extent Report
+        CustomExtentReport extentReport = new CustomExtentReport()
+                .withKarateResult(results)
+                .withReportDir(reportDir)
+                .withReportTitle("Karate Test Execution Report");
+        extentReport.generateExtentReport();
 
 
         return String.format(
-                "Scénarios Karate exécutés : %d réussis, %d échoués",
-                results.getScenariosPassed(), results.getScenariosFailed()
-        );
-    }
-
-    @GetMapping("/runcsv")
-    public String runKarateTestscsv() {
-        Results results = Runner.path("src/test/resources/karate/testcsv.feature").parallel(1);
-
-        return String.format(
-                "Scénarios Karate exécutés : %d réussis, %d échoués",
+                "Scénarios Karate exécutés : %d réussis, %d échoués. Rapport généré dans target/karate-reports/ExtentReport.html",
                 results.getScenariosPassed(), results.getScenariosFailed()
         );
     }
@@ -120,5 +117,10 @@ public class KarateTestController {
     }
 
 }
+
+
+
+
+
 
 
